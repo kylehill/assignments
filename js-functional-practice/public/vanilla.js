4999cbaa64dda81f140aef2a8a2b66ba70695d8e
Kyle Hill <kylehill@gmail.com>
2015-02-03 22:08:16 -0500

    Adds first part of js functional practice

---
+++ js-functional-practice/public/vanilla.js
@@ -0,0 +1,44 @@
+/*
+  This function should accept an array of strings as a parameter.
+  It should return the array, with all strings coverted to uppercase.
+
+  It should not use underscore.
+*/
+var uppercase = function(array){
+
+}
+
+/*
+  This function should accept an array of numbers as a parameter.
+  It should return the array, with all numbers sorted in descending order.
+
+  It should not use underscore.
+*/
+var sort = function(array){
+
+}
+
+/*
+  This function should accept an array of objects as a parameter.
+  It should return an array of objects that contain the property "name".
+*/
+var namesOnly = function(array) {
+
+}
+
+/*
+  This function should accept an array of numbers as a parameter.
+  It should return the first number that is evenly divisible by 5.
+*/
+var firstFive = function(array) {
+
+}
+
+/*
+  This function should accept an array of numbers a parameter.
+  It should return the sum of all ODD numbers in the array.
+    (Ignore the even numbers.)
+*/
+var sumOdd = function(array) {
+
+}
