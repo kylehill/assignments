bf252a7e6e0288a6224aeffb32dacb406367496c
Kyle Hill <kylehill@gmail.com>
2015-02-20 18:30:43 -0500

    Adds Ironmon prototypes assignment

---
+++ ironmon-prototypes/public/index.js
@@ -0,0 +1,19 @@
+var Ironmon = function(name) {
+
+}
+
+Ironmon.prototype.attack = function(opponent) {
+
+}
+
+Ironmon.prototype.heal = function() {
+
+}
+
+Ironmon.prototype.train = function() {
+
+}
+
+Ironmon.prototype.active = function() {
+
+}
