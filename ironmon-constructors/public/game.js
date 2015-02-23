9b007fcf6801ef9aaa80bbb39d4d4222928b6876
Kyle Hill <kylehill@gmail.com>
2015-02-22 22:37:08 -0500

    Adds Ironmon readme and comments

--- ironmon-constructors/public/game.js
+++ ironmon-constructors/public/game.js
@@ -1,7 +1,23 @@
+/*
+  This class represents a battle between characters in a video game.
+
+  It contains one default properties:
+    - turn (which starts as true)
+    
+  It also contains a two properties partially passed in as parameters.
+    - left (an instance of the Ironmon class -- the name is passed in as leftName)
+    - right (also an instance of the Ironmon class -- the name is passed in as rightName)
+
+  You'll need to create Ironmon instances in this constructor.
+*/
 var Game = function(leftName, rightName) {
 
 }
 
-Game.prototype.takeTurn = function(action) {
+/*
+  If this Game's turn property is true, it becomes false.
+  Otherwise, it becomes true.
+*/
+Game.prototype.takeTurn = function() {
 
 }
--- ironmon-constructors/public/ironmon.js
+++ ironmon-constructors/public/ironmon.js
@@ -1,19 +1,53 @@
+/*
+  This class represents a character in a video game.
+
+  It contains two default properties:
+    - health (which starts at 25)
+    - power (which starts at 1)
+
+  It also contains a property passed in as a parameter:
+    - name
+*/
 var Ironmon = function(name) {
 
 }
 
+/*
+  This function determines a random *integer* between 1 and 5.
+  
+  It adds that number to the health of the Ironmon.
+
+  An Ironmon's health cannot go above 25. If healing would 
+    make the Ironmon's health go above 25, it becomes 25 instead.
+*/
 Ironmon.prototype.heal = function() {
 
 }
 
+/*
+  This function adds one to the power of the Ironmon.
+*/
 Ironmon.prototype.train = function() {
 
 }
 
+/*
+  This function returns true if the Ironmon's health is greater than 0.
+
+  Otherwise, it returns false.
+*/
 Ironmon.prototype.active = function() {
 
 }
 
+/*
+  This function accepts another, opponent Ironmon as a parameter.
+
+  It reduces the health of the opponent Ironmon by a random *integer*
+    between 1 and whatever this Ironmon's power is.
+
+  This function returns the amount of damage dealt.
+*/
 Ironmon.prototype.attack = function(opponent) {
   
 }
